379bd42575cda0996e37b1f204e4caab33a73587
jayankohli <73661056+jayankohli@users.noreply.github.com>
2025-10-08 10:54:33 +0530

    Add files via upload

---
+++ simple_math_operations.py
@@ -0,0 +1,10 @@
+1a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+a = float(a)
+b = float(b)
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
